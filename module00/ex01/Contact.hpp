069ab2c6c7d66dae902c47b4b482eee29ae61182
= <kuba.kaczmarski312@gmail.com>
2022-06-19 23:41:11 +0200

    add_contacts function added

--- module00/ex01/Contact.hpp
+++ module00/ex01/Contact.hpp
@@ -6,7 +6,7 @@
 /*   By: jkaczmar <jkaczmar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/19 21:29:46 by jkaczmar          #+#    #+#             */
-/*   Updated: 2022/06/19 21:30:32 by jkaczmar         ###   ########.fr       */
+/*   Updated: 2022/06/19 23:36:13 by jkaczmar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,11 @@
 class Contact
 {
     public:
-
-    private:
-    
+    std::string first_name_;
+    std::string last_name_;
+    std::string nickname_;
+    std::string phone_num_;
+    std::string darkest_secret_;
 };
 
 #endif
--- module00/ex01/PhoneBook.hpp
+++ module00/ex01/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: jkaczmar <jkaczmar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/19 21:20:48 by jkaczmar          #+#    #+#             */
-/*   Updated: 2022/06/19 23:32:45 by jkaczmar         ###   ########.fr       */
+/*   Updated: 2022/06/19 23:40:26 by jkaczmar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,17 @@
 #include "Contact.hpp"
 #include <string>
 
-class Phonebook
+class PhoneBook
 {
     public:
+        PhoneBook()
+        {
+            contact_index_ = 0;
+        }
         void add_contact(std::string first_name, std::string last_name, std::string nickname, std::string phone_num, std::string darkest_secret);
     private:
     Contact arr[8];
-    int contact_index = 0;
+    int contact_index_;
 };
 
 #endif
