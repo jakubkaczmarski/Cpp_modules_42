88a00df0d0421652fd0ae2d882935dee472540ac
= <kuba.kaczmarski312@gmail.com>
2022-07-23 12:29:14 +0200

    ex00 done

--- module02/ex00/fixed.cpp
+++ module02/ex00/fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: jkaczmar <jkaczmar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/23 12:12:52 by jkaczmar          #+#    #+#             */
-/*   Updated: 2022/07/23 12:27:03 by jkaczmar         ###   ########.fr       */
+/*   Updated: 2022/07/23 12:28:48 by jkaczmar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,14 @@
 Fixed::Fixed()
 {
     std::cout << "Default constructor called" << std::endl;
-    setRawBits(0);
+    this->num_val_ = 0;
 };
 
 Fixed &Fixed::operator=(const Fixed &other)
 {
     std::cout << "Copy assignment operator called" << std::endl;
-    this->num_val_ = other.num_val_;
-    return *this;   
+    this->num_val_ = other.getRawBits();
+    return *this;
 }
 
 Fixed::~Fixed()
