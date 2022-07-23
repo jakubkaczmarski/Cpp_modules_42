b54b6f893678a841e61c48a808773f9b993a6696
= <kuba.kaczmarski312@gmail.com>
2022-07-23 09:16:28 +0200

    Ex 04

--- module01/ex04/main.cpp
+++ module01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jkaczmar <jkaczmar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/23 02:21:00 by jkaczmar          #+#    #+#             */
-/*   Updated: 2022/07/23 02:53:57 by jkaczmar         ###   ########.fr       */
+/*   Updated: 2022/07/23 09:15:45 by jkaczmar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,34 +14,33 @@
 #include <iostream>
 #include <fstream>
 
-void    replace_line(std::string line, std::string to_replace, std::string replace_by)
+std::string    replace_line(std::string line, std::string to_replace, std::string replace_by)
 {
-    if(replace_by[0]){}
+    // if(replace_by[0]){}
     // check_if_for_characs(std::string line,);
-    std::cout << to_replace << std::endl;
-    std::cout << replace_by << std::endl;
+    // std::cout << to_replace << std::endl;
+    // std::cout << replace_by << std::endl;
+    // std::cout << line << std::endl;
     for(int i = 0; i < (int)line.length(); i++)
     {
-        if(line[i] == to_replace[0])
+        if(line.compare(i, to_replace.length(), to_replace) == 0)
         {
-            int temp_i = i;
-            for(int j = 0; j < (int)to_replace.length() && i < (int)line.length(); j++)
+            std::string temp;
+            std::cout << line.substr(i, to_replace.length()) << std::endl;
+            int j;
+            for(j = 0; j < i; j++)
             {
-                if(line[i] != to_replace[j])
-                {
-                    i = temp_i;
-                    break;
-                }
-                if(line[i] == to_replace[j] && j + 1 == (int)to_replace.length())
-                {
-                    std::cout << "Found " << to_replace << std::endl;
-                    break;
-                }
-                j++;
-                i++;   
+                temp.push_back(line[j]);
             }
+            temp.append(replace_by);
+            for(j += to_replace.length(); j < (int)line.length(); j++)
+            {
+                temp.push_back(line[j]);
+            }
+            line = temp;
         }
     }
+    return line;
 }
 
 int main(int argc, char **argv)
@@ -68,8 +67,7 @@ int main(int argc, char **argv)
         }
         while(getline(file, line))
         {
-            replace_line(line, to_replace, replace_by);
-            new_file << line;
+            new_file << replace_line(line, to_replace, replace_by);
             new_file << std::endl;
         }
     }else{
