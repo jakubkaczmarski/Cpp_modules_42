9016a54ef2ffab5330a2fee2f2371cd9b6bd24c0
= <kuba.kaczmarski312@gmail.com>
2022-07-26 01:44:02 +0200

    Comparision and value manipulating operators added

---
+++ module02/ex02/fixed.cpp
@@ -0,0 +1,161 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkaczmar <jkaczmar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/23 12:12:52 by jkaczmar          #+#    #+#             */
+/*   Updated: 2022/07/26 01:42:18 by jkaczmar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fixed.hpp"
+
+Fixed::Fixed() :   num_val_(0)
+{
+    std::cout << "Default constructor called" << std::endl;
+  
+};
+
+Fixed::Fixed(const Fixed &copy)
+{
+    std::cout << "Copy constructor called" << std::endl;
+    *this = copy;
+}
+
+int Fixed::operator >(const Fixed &other)
+{
+    if (num_val_ < other.num_val_)
+    {
+        return 1;
+    }else{
+        return 0;
+    }
+}
+int Fixed::operator <(const Fixed &other)
+{
+    if (num_val_ > other.num_val_)
+    {
+        return 1;
+    }else{
+        return 0;
+    }
+}
+int Fixed::operator <=(const Fixed &other)
+{
+    if (num_val_ >= other.num_val_)
+    {
+        return 1;
+    }else{
+        return 0;
+    }
+}
+int Fixed::operator >=(const Fixed &other)
+{
+    if (num_val_ <= other.num_val_)
+    {
+        return 1;
+    }else{
+        return 0;
+    }
+}
+int Fixed::operator == (const Fixed &other)
+{
+    if(other.num_val_ == num_val_)
+    {
+        return 1;
+    }else{
+        return 0;
+    }
+}
+int Fixed::operator != (const Fixed &other)
+{
+    if(other.num_val_ != num_val_)
+    {
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+
+Fixed Fixed::operator +(const Fixed &other)
+{
+    Fixed res;
+    res.num_val_ = other.num_val_ + num_val_;
+    return res;
+}
+
+Fixed Fixed::operator -(const Fixed &other)
+{
+    Fixed res;
+    res.num_val_ = other.num_val_ - num_val_;
+    return res;
+}
+
+Fixed Fixed::operator *(const Fixed &other)
+{
+    Fixed res;
+    res.num_val_ = other.num_val_ * num_val_;
+    return res;
+}
+
+Fixed Fixed::operator /(const Fixed &other)
+{
+    Fixed res;
+    res.num_val_ = other.num_val_ / num_val_;
+    return res;
+}
+
+Fixed &Fixed::operator=(const Fixed &other)
+{
+    std::cout << "Copy assignment operator called" << std::endl;
+    this->num_val_ = other.num_val_;
+    return *this;
+}
+
+Fixed::~Fixed()
+{
+    std::cout << "Destructor called" << std::endl;
+};
+
+void    Fixed::setRawBits(int const num)
+{
+    std::cout << "setRawBits member function called" << std::endl;
+    this->num_val_ = num;
+}
+
+int Fixed::getRawBits() const
+{
+    std::cout << "getRawBits member function called" << std::endl;
+    return this->num_val_;
+}
+
+int Fixed::toInt() const
+{
+    return ((int)this->num_val_ >> this->num_of_frac_bits);
+}
+
+float Fixed::toFloat() const
+{
+    return (float)((float)this->num_val_ / (float)(1 << this->num_of_frac_bits));
+}
+Fixed::Fixed(int num) 
+{
+    this->num_val_ = num  << this->num_of_frac_bits;
+}
+
+Fixed::Fixed(float num)
+{
+    this->num_val_ = 0;
+    this->num_val_ = roundf(num * 256);
+    // printf("Send help %d", num_val_);
+    printf("%f\n",this->toFloat());
+    // printf("Zium %d \n", this->num_val_);
+}
+std::ostream& operator<<(std::ostream &os, const Fixed& fix)
+{
+    os << fix.toFloat();
+    return os;
+}
---
+++ module02/ex02/fixed.hpp
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkaczmar <jkaczmar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/23 11:10:39 by jkaczmar          #+#    #+#             */
+/*   Updated: 2022/07/26 01:43:46 by jkaczmar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+#include <iostream>
+#include <string>
+#include <cmath>
+
+class Fixed
+{
+    public:
+    Fixed();
+    Fixed(const int num);
+    Fixed(const float num);
+    Fixed(const Fixed &copy);
+    Fixed &operator=(const Fixed &other);
+    //Comparision
+    int operator > (const Fixed &other);
+    int operator < (const Fixed &other);
+    int operator <= (const Fixed &other);
+    int operator >= (const Fixed &other);
+    int operator == (const Fixed &other);
+    int operator != (const Fixed &other);
+
+    //Value manipulating operators
+    Fixed operator +(const Fixed &other);
+    Fixed operator -(const Fixed &other);
+    Fixed operator *(const Fixed &other);
+    Fixed operator /(const Fixed &other);
+    
+    ~Fixed();
+    int     getRawBits(void) const;
+    void     setRawBits(int const raw);
+    int        toInt() const;
+    float   toFloat() const;
+    private:
+    int num_val_;
+    static const int num_of_frac_bits = 8;
+};
+
+std::ostream& operator<<(std::ostream &os, const Fixed& fix);
+#endif
