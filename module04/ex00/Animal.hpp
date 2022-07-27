70049555d402572c72efb898c22464b8a72d5792
= <kuba.kaczmarski312@gmail.com>
2022-07-27 02:30:33 +0200

    Animal class added

---
+++ module04/ex00/Animal.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkaczmar <jkaczmar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/27 02:28:44 by jkaczmar          #+#    #+#             */
+/*   Updated: 2022/07/27 02:29:44 by jkaczmar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ANIMAL_HPP
+# define ANIMAL_HPP
+
+#include <iostream>
+
+class Animal
+{
+    public:
+        Animal();
+        ~Animal();
+    protected:
+        std::string type;
+}
+
+
+#endif
