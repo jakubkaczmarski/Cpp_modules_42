2d01d68c300372c79b41e6cee437e063a3ee6abe
= <kuba.kaczmarski312@gmail.com>
2022-07-28 01:02:25 +0200

    ex01 done

--- module04/ex01/main.cpp
+++ module04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jkaczmar <jkaczmar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/27 02:47:37 by jkaczmar          #+#    #+#             */
-/*   Updated: 2022/07/28 00:57:46 by jkaczmar         ###   ########.fr       */
+/*   Updated: 2022/07/28 01:01:44 by jkaczmar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,20 @@ int main()
 {
     const Animal* j = new Dog();
     const Animal* i = new Cat();
+    Animal *arr[100];
+    for(int x = 0; x < 50; x++)
+    {
+        arr[x] = new Dog();
+    }
+    for(int x = 50; x < 100; x++)
+    {
+        arr[x] = new Cat();
+    }
+    //Delete the array
+    for(int x = 0; x < 100; x++)
+    {
+        delete arr[x];    
+    }
     delete j;//should not create a leak
     delete i;
     return 0;
