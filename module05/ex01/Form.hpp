81074b8d2bbbd3066079499ed1dcd2851819e920
= <kuba.kaczmarski312@gmail.com>
2022-07-30 01:43:55 +0200

    ex01 template

---
+++ module05/ex01/Form.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Form.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkaczmar <jkaczmar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/30 01:40:22 by jkaczmar          #+#    #+#             */
+/*   Updated: 2022/07/30 01:43:47 by jkaczmar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FORM_HPP
+#define FORM_HPP
+
+class Form
+{
+    public:
+    
+
+    private:
+        const std::name name_;
+        bool    signed_;
+        const int         grade_to_sign;
+        const int         grade_to_exec;
+}
+
+#endif
