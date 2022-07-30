e95741026af0835a29d34e5d5a662f073b679479
= <kuba.kaczmarski312@gmail.com>
2022-07-30 02:32:42 +0200

    ex02 template

---
+++ module05/ex02/Bureaucrat.cpp
@@ -0,0 +1,116 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkaczmar <jkaczmar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/28 02:11:01 by jkaczmar          #+#    #+#             */
+/*   Updated: 2022/07/30 01:40:02 by jkaczmar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat() : name("Def_name"), grade(12)
+{
+    std::cout << "Bureaucrat default constructor ran" << std::endl;
+}
+Bureaucrat::Bureaucrat(const Bureaucrat& cp)
+{
+     std::cout << "Bureaucrat copy_constructor called" << std::endl;
+    *this = cp;
+}
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat&cp)
+{
+    this->grade = cp.getGrade();
+    this->name = cp.getName();
+    return *(this);
+}
+Bureaucrat::Bureaucrat(const std::string name, int grade) 
+: name(name)
+{
+    std::cout << "Bureaucrat parameter constructor run" << std::endl;
+    if(grade < 1)
+    {
+        throw Bureaucrat::GradeTooHighException();
+    }else if(grade > 150)
+    {
+        throw Bureaucrat::GradeTooLowException();
+    }else{
+        this->grade = grade;
+    }
+}
+
+std::string    Bureaucrat::getName() const
+{
+    return this->name;
+}
+
+int                 Bureaucrat::getGrade() const
+{
+    return this->grade;   
+}
+Bureaucrat::~Bureaucrat()
+{
+    std::cout << "Bureaucrat destructor run" << std::endl;
+}
+Bureaucrat &Bureaucrat::operator++()
+{
+    if(this->grade - 1 < 1)
+    {
+        throw Bureaucrat::GradeTooHighException();
+        return *this;
+    }
+    grade--;
+    return *this;
+}
+
+Bureaucrat Bureaucrat::operator++(int)
+{
+    const Bureaucrat old = *this;
+    if(this->grade - 1 < 1)
+    {
+        throw Bureaucrat::GradeTooHighException();
+        return old;
+    }
+    --(this->grade);
+    return old;
+}
+
+Bureaucrat &Bureaucrat::operator--()
+{
+    if(this->grade + 1 > 150)
+    {
+        throw Bureaucrat::GradeTooLowException();
+        return *this;
+    }
+    grade++;
+    return *this;
+}
+
+Bureaucrat Bureaucrat::operator--(int)
+{
+    Bureaucrat old;
+    // old = *this;
+    if(this->grade + 1 > 150)
+    {
+        throw Bureaucrat::GradeTooLowException();
+        return old;
+    }
+    ++(this->grade);
+    return old;
+} 
+const char * Bureaucrat::GradeTooHighException::what(void) const throw()
+{
+    return ("Grade is too high :O ");
+} 
+const char * Bureaucrat::GradeTooLowException::what(void) const throw()
+{
+    return  ("Grade too low :O");
+}
+std::ostream& operator<<(std::ostream &os, const Bureaucrat& fix)
+{
+    os << fix.getName() << ", bureaucrat grade " << fix.getGrade();
+    return os;
+}
---
+++ module05/ex02/Form.cpp
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Form.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkaczmar <jkaczmar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/30 01:51:12 by jkaczmar          #+#    #+#             */
+/*   Updated: 2022/07/30 02:30:50 by jkaczmar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Form.hpp"
+
+Form::Form() : name_("Def_name"), signed_(false),
+    grade_to_sign_(5) ,grade_to_exec_(5)
+{
+    std::cout << "Form default constructor ran" << std::endl;
+}
+
+Form::Form(std::string name, int grade_exec, int grade_sign)
+    : name_(name),signed_(false), grade_to_sign_(grade_sign),  grade_to_exec_(grade_exec)
+{
+    std::cout << "Paramatirized Form constructor ran" << std::endl;
+    if(grade_to_exec_ < 1 || grade_to_sign_ < 1)
+    {
+        throw Bureaucrat::GradeTooHighException();
+    }else if(grade_to_exec_ > 150 || grade_to_sign_ > 150)
+    {
+        throw Bureaucrat::GradeTooLowException();
+    }
+}
+
+Form::Form(const Form &cp) :
+name_(cp.name_), signed_(cp.signed_), 
+grade_to_sign_(cp.grade_to_sign_), 
+grade_to_exec_(cp.grade_to_exec_)
+{
+    std::cout << "Form copy_constructor called" << std::endl;
+    *this = cp;
+}
+
+Form& Form::operator=(const Form&cp)
+{
+    this->signed_ = cp.signed_;
+    return *(this);
+}
+
+int Form::get_grade_to_sign()
+{
+    return this->grade_to_sign_;
+}
+
+int Form::get_grade_to_exec()
+{
+    return this->grade_to_exec_;
+}
+
+std::string Form::getName()
+{
+    return this->name_;
+}
+
+bool    Form::get_if_signed()
+{
+    return this->signed_;
+}
+
+const char * Form::GradeTooHighException::what(void) const throw()
+{
+    return ("Grade is too high :O ");
+} 
+const char * Form::GradeTooLowException::what(void) const throw()
+{
+    return  ("Grade too low :O");
+}
+
+void    Form::beSigned(Bureaucrat &b)
+{
+    if(b.getGrade() > this->grade_to_sign_)
+    {
+        throw Form::GradeTooLowException();
+    }else{
+        this->signed_ = true;
+    }
+}
+
+std::ostream& operator<<(std::ostream &os, Form& fix)
+{
+    if(fix.get_if_signed())
+        os << "Bureaucrat " << fix.getName() << " with grades to exec " <<
+                fix.get_grade_to_exec() << " and grade_to_sign " << fix.get_grade_to_sign() 
+                    << " signed the document" << std::endl;
+    else{
+        os << "Bureaucrat " << fix.getName() << " with grades to exec " <<
+                fix.get_grade_to_exec() << " and grade_to_sign " << fix.get_grade_to_sign() 
+                    << " didn't sign the document" << std::endl;
+    }
+    return os;
+                
+}
