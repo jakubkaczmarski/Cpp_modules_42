ffcd73e28772f3d78de63ea0a81d7d970af9c23c
= <kuba.kaczmarski312@gmail.com>
2022-07-30 02:31:05 +0200

    ex01 done

--- module05/ex01/Form.cpp
+++ module05/ex01/Form.cpp
@@ -6,20 +6,20 @@
 /*   By: jkaczmar <jkaczmar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/30 01:51:12 by jkaczmar          #+#    #+#             */
-/*   Updated: 2022/07/30 02:01:49 by jkaczmar         ###   ########.fr       */
+/*   Updated: 2022/07/30 02:30:50 by jkaczmar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Form.hpp"
 
-Form::Form() : name_("Def_name"), 
+Form::Form() : name_("Def_name"), signed_(false),
     grade_to_sign_(5) ,grade_to_exec_(5)
 {
     std::cout << "Form default constructor ran" << std::endl;
 }
 
 Form::Form(std::string name, int grade_exec, int grade_sign)
-    : name_(name), grade_to_sign_(grade_sign),  grade_to_exec_(grade_exec)
+    : name_(name),signed_(false), grade_to_sign_(grade_sign),  grade_to_exec_(grade_exec)
 {
     std::cout << "Paramatirized Form constructor ran" << std::endl;
     if(grade_to_exec_ < 1 || grade_to_sign_ < 1)
@@ -31,7 +31,10 @@ Form::Form(std::string name, int grade_exec, int grade_sign)
     }
 }
 
-Form::Form(const Form &cp)
+Form::Form(const Form &cp) :
+name_(cp.name_), signed_(cp.signed_), 
+grade_to_sign_(cp.grade_to_sign_), 
+grade_to_exec_(cp.grade_to_exec_)
 {
     std::cout << "Form copy_constructor called" << std::endl;
     *this = cp;
@@ -39,10 +42,60 @@ Form::Form(const Form &cp)
 
 Form& Form::operator=(const Form&cp)
 {
-    // Form n_form(signed)
-    // this->grade_to_sign_ = cp.get_grade_to_sign();
-    // this->grade_to_exec_ = cp.get_grade_to_exec();
-    // this->signed_ = get_if_signed();
-    // this->name_ = cp.getName();
+    this->signed_ = cp.signed_;
     return *(this);
+}
+
+int Form::get_grade_to_sign()
+{
+    return this->grade_to_sign_;
+}
+
+int Form::get_grade_to_exec()
+{
+    return this->grade_to_exec_;
+}
+
+std::string Form::getName()
+{
+    return this->name_;
+}
+
+bool    Form::get_if_signed()
+{
+    return this->signed_;
+}
+
+const char * Form::GradeTooHighException::what(void) const throw()
+{
+    return ("Grade is too high :O ");
+} 
+const char * Form::GradeTooLowException::what(void) const throw()
+{
+    return  ("Grade too low :O");
+}
+
+void    Form::beSigned(Bureaucrat &b)
+{
+    if(b.getGrade() > this->grade_to_sign_)
+    {
+        throw Form::GradeTooLowException();
+    }else{
+        this->signed_ = true;
+    }
+}
+
+std::ostream& operator<<(std::ostream &os, Form& fix)
+{
+    if(fix.get_if_signed())
+        os << "Bureaucrat " << fix.getName() << " with grades to exec " <<
+                fix.get_grade_to_exec() << " and grade_to_sign " << fix.get_grade_to_sign() 
+                    << " signed the document" << std::endl;
+    else{
+        os << "Bureaucrat " << fix.getName() << " with grades to exec " <<
+                fix.get_grade_to_exec() << " and grade_to_sign " << fix.get_grade_to_sign() 
+                    << " didn't sign the document" << std::endl;
+    }
+    return os;
+                
 }
