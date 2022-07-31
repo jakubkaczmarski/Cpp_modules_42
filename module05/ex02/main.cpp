c2132e2bd1a8fc20a83f5c1edcfe41d3715d1816
= <kuba.kaczmarski312@gmail.com>
2022-07-31 17:23:02 +0200

    update

--- module05/ex02/main.cpp
+++ module05/ex02/main.cpp
@@ -6,16 +6,20 @@
 /*   By: jkaczmar <jkaczmar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/28 01:14:39 by jkaczmar          #+#    #+#             */
-/*   Updated: 2022/07/31 16:45:42 by jkaczmar         ###   ########.fr       */
+/*   Updated: 2022/07/31 17:22:44 by jkaczmar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
-
+#include "PresidentialPardonForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "ShrubberyCreationForm.hpp"
+#include "Form.hpp"
 
 int main()
 {
     Bureaucrat bob("Bob", 1);
+
     try{
         bob++;   
     }catch(std::exception &e){
