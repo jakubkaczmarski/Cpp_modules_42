c59fc72bd762039d28aa1f5c325a3eb8b52012bc
= <kuba.kaczmarski312@gmail.com>
2022-08-02 03:24:19 +0200

    Template mod05

--- module06/ex00/main.cpp
+++ module06/ex00/main.cpp
@@ -6,13 +6,12 @@
 /*   By: jkaczmar <jkaczmar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/02 00:55:38 by jkaczmar          #+#    #+#             */
-/*   Updated: 2022/08/02 01:28:30 by jkaczmar         ###   ########.fr       */
+/*   Updated: 2022/08/02 03:18:41 by jkaczmar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-
-
+#include <cstdlib>
 
 
 int main(int argc, char **argv)
@@ -25,10 +24,21 @@ int main(int argc, char **argv)
         return (0);
     }
     thingy = argv[1];
-    int i = 0;
-    for(; thingy.length() > 1 && thingy[i] && thingy[i] == ' '; i++)
-    {    
-    }
-    thingy = thingy.substr(i, thingy.length() -1);
-    std::cout << thingy << std::endl;
+    // float i;
+    // int i = 0;
+    // for(; thingy.length() > 1 && thingy[i] && thingy[i] == ' '; i++)
+    // {    
+    // }
+    // thingy = thingy.substr(i, thingy.length() -1);
+    // if(thingy.length() > 1)
+    // {
+    //     std::cout << "Wrong thingy" << std::endl;
+    //     return (0);
+    // }
+    double num = std::stod(thingy);
+
+    std::cout << "float: " << num << "f" << std::endl;
+    std::cout << "char: " << thingy[0] << std::endl;
+    // std::cout << "int: " << int(thingy) << std::endl;
+    // std::cout << "double: " << double(thingy) << std::endl;
 }
