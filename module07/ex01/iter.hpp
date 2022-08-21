c91c593422c8a3238827ffa53e6f5de3fb15b968
= <kuba.kaczmarski312@gmail.com>
2022-08-21 23:02:27 +0200

    ex01 done

--- module07/ex01/iter.hpp
+++ module07/ex01/iter.hpp
@@ -6,13 +6,24 @@
 /*   By: jkaczmar <jkaczmar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/21 01:52:05 by jkaczmar          #+#    #+#             */
-/*   Updated: 2022/08/21 01:53:17 by jkaczmar         ###   ########.fr       */
+/*   Updated: 2022/08/21 23:02:09 by jkaczmar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITER_HPP
 #define ITER_HPP
-
-template<typename Type> void max(Type &address, int lenght, *Type() );
-
+#include <iostream>
+template <typename T>
+void func(T *arr_ptr, int lenght, void(func)(T))
+{
+    for(int i = 0; i < lenght; i++)
+    {
+        func(arr_ptr[i]); 
+    }
+}
+template <typename T>
+void special_func(T el)
+{
+    std::cout << el << std::endl;
+}
 #endif
