7b5aad1002d0556751b4379e4712a4cdfad4c8ad
= <kuba.kaczmarski312@gmail.com>
2022-08-23 00:52:46 +0200

    update

--- module07/ex02/Array.cpp
+++ module07/ex02/Array.cpp
@@ -6,22 +6,56 @@
 /*   By: jkaczmar <jkaczmar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/21 23:08:31 by jkaczmar          #+#    #+#             */
-/*   Updated: 2022/08/21 23:28:52 by jkaczmar         ###   ########.fr       */
+/*   Updated: 2022/08/23 00:52:38 by jkaczmar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Array.hpp"
 
 template<typename T>
-Array<T>::Array() : arr(0), size(0)
+Array<T>::Array() : arr(0), size_(0)
 {
 }
 template<typename T>
-Array<T>::Array(unsigned int val) : arr(new T[val]), size(val)
+Array<T>::Array(unsigned int val) : arr(new T[val]), size_(val)
 {
 }
 template<typename T>
-Array<T>::Array(Array &cp) : size(cp.size), arr(0)
+Array<T>::Array(Array &cp) : size_(cp.size), arr(0)
 {
     *this = cp;
+}
+template<typename T>
+const  char * Array<T>::Exception_thingy::what()
+{
+    return "Index out of bounds"; 
+}
+template<typename T>
+T Array<T>::operator[](int val)
+{
+    try{
+        if(val > 0 && val < this->size_)
+        {
+            return this->arr[val];
+        }else{
+            throw Exception_thingy();
+        
+        }
+    }catch(Exception_thingy m)
+    {
+        std::cout << m.what() << std::endl;
+    }
+    
+}
+
+template<typename T>
+int Array<T>::size()
+{
+    return size;
+}
+
+template<typename T>
+Array<T> Array<T>::operator=(const Array<T> &arr)
+{
+    
 }
--- module07/ex02/Array.hpp
+++ module07/ex02/Array.hpp
@@ -6,13 +6,15 @@
 /*   By: jkaczmar <jkaczmar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/21 23:05:23 by jkaczmar          #+#    #+#             */
-/*   Updated: 2022/08/21 23:27:36 by jkaczmar         ###   ########.fr       */
+/*   Updated: 2022/08/23 00:50:24 by jkaczmar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
 
+#include <exception>
+#include <iostream>
 
 template<typename T>
 class Array
@@ -21,9 +23,15 @@ class Array
     Array();
     Array(unsigned int n);
     Array(Array &cp);
+    T operator[](int val);
+    class Exception_thingy : public std::exception{
+        public:
+       const char *what();
+    };
+    int size();
     private:
     T *arr;
-    int size;
+    int size_;
 };
 
 
