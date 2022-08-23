6c80b6987007eb1db2d39bf18729d4f437099e19
Jakub Kaczmarski <kuba.kaczmarski312@gmail.com>
2022-08-23 15:25:34 +0200

    Ex00 done

--- module08/ex00/easyfind.hpp
+++ module08/ex00/easyfind.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   easyfind.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jkaczmar <jkaczmar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jkaczmar <jkaczmar@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/23 01:08:59 by jkaczmar          #+#    #+#             */
-/*   Updated: 2022/08/23 14:06:09 by jkaczmar         ###   ########.fr       */
+/*   Updated: 2022/08/23 15:22:41 by jkaczmar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,9 @@ typename T::iterator   easyFind(T target, int thingy)
     typename T::iterator it = std::find(begin(target), end(target), thingy);
     if(it == end(target))
     {
+        throw Exc();
         return it;
     }else{
-        throw Exc();
+        return it;
     }
 }
--- module08/ex00/main.cpp
+++ module08/ex00/main.cpp
@@ -3,16 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jkaczmar <jkaczmar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jkaczmar <jkaczmar@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/23 01:08:56 by jkaczmar          #+#    #+#             */
-/*   Updated: 2022/08/23 14:04:28 by jkaczmar         ###   ########.fr       */
+/*   Updated: 2022/08/23 15:25:21 by jkaczmar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
+#include <array>
+#include <vector>
+#include <iostream>
 
 int main()
 {
-    
+    std::vector<int>vec;
+    vec.push_back(12);
+    vec.push_back(32);
+    vec.push_back(123);
+    vec.push_back(122);
+    if(vec[0]){}
+    std::cout << *easyFind(vec, 122) << std::endl;
 }
