04f9805336876d92476c4f13e1a052b7ea956111
= <kuba.kaczmarski312@gmail.com>
2022-08-24 13:58:37 +0200

    ex00 ex01

--- module07/ex01/iter.hpp
+++ module07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: jkaczmar <jkaczmar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/21 01:52:05 by jkaczmar          #+#    #+#             */
-/*   Updated: 2022/08/21 23:02:09 by jkaczmar         ###   ########.fr       */
+/*   Updated: 2022/08/24 13:57:37 by jkaczmar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,18 @@
 #define ITER_HPP
 #include <iostream>
 template <typename T>
-void func(T *arr_ptr, int lenght, void(func)(T))
+void iter(T *arr_ptr, int lenght, void(func)(const T&))
 {
     for(int i = 0; i < lenght; i++)
     {
         func(arr_ptr[i]); 
     }
 }
-template <typename T>
-void special_func(T el)
+
+template<typename T>
+void print(T const &x)
 {
-    std::cout << el << std::endl;
+       std::cout << x << std::endl;
+       return ;
 }
 #endif
--- module07/ex01/main.cpp
+++ module07/ex01/main.cpp
@@ -6,14 +6,33 @@
 /*   By: jkaczmar <jkaczmar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/21 01:51:43 by jkaczmar          #+#    #+#             */
-/*   Updated: 2022/08/21 23:01:51 by jkaczmar         ###   ########.fr       */
+/*   Updated: 2022/08/24 13:58:00 by jkaczmar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 
+class Awesome
+{
+       public:
+       Awesome(void) : _n(42){return ;};
+       int get(void) const{return this->_n;}
+       private:
+       int    _n;
+};
+std::ostream &operator <<(std::ostream &o, Awesome const &rhs)
+{
+       o << rhs.get();
+       return o;
+}
+
+
 int main()
 {
-       int arr[5] = {3,2,4,1,0};
-       func(&arr[0], 5,special_func);
+       int tab[] = {0,1,2,3,4};
+       Awesome tab2[5];
+       iter(tab, 5, print);
+       iter(tab2, 5, print);
+       // int arr[5] = {3,2,4,1,0};
+       // iter(&arr[0], 5,special_func);
 }
